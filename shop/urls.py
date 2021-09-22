84909a115cc6b89370d6b4da02a362495b1cbee5
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-22 14:36:56 +0530

    just setup basic views that may be required

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from . import views
 
 app_name = 'shop'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('orderhistory/', views.order_history, name='orders'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'shop/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'shop/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'shop/checkout.html', context)
+
+def order_history(request):
+    context = {}
+    return render(request, 'shop/orderhistory.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'shop/login.html', context)
+
+def signup(request):
+    context = {}
+    return render(request, 'shop/signup.html', context)
+
