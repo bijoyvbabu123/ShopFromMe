695ddf06ea28924c7ac68813305cd75583582a77
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-23 20:44:03 +0530

    created the Product model and registered it with the admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'available']
+
+
+
+
+admin.site.register(models.Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    AVAILABILITY = [
+        ("YES", "yes"),
+        ("NO", "no")
+    ]
+
+    name = models.CharField(max_length=100, verbose_name="Product Name", blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, verbose_name="Price")
+    # the imagefield will save the images to the image_root in the settings since no path is specified. the default is also from the same path
+    image = models.ImageField(default='placeholder.png', blank=False, verbose_name="Product Image")
+    available = models.CharField(max_length=5, choices=AVAILABILITY, default=AVAILABILITY[0][1], blank=False, verbose_name="Available ?")
+
+    def __str__(self):
+        return self.name
+    
