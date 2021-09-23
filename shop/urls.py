689ac47a64592a5975ea468951724032622e7331
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-23 20:42:06 +0530

    added paths for the medias in urlpatterns

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 app_name = 'shop'
 
 urlpatterns = [
@@ -12,3 +15,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_user, name='logout'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
