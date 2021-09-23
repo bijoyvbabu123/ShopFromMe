e05a508326304e42192a3672cc88eced5f52fb21
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-23 19:58:16 +0530

    added Media root path

--- ShopFromMe/settings.py
+++ ShopFromMe/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticassets")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "shop/static/shop/")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
