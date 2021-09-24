3f9c6f6fc7c5b20de30b639cb3f0bf07f0d22aa7
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-24 15:48:45 +0530

    created a loginform class for overriding the django builtin authentication form for fixing a bug. This class is not used for any form modification

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.forms import ModelForm, fields
 from django import forms
 
@@ -9,3 +10,28 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+# overriding the builtin form since it had a bug that did'nt show the inactive user error message
+class LogInForm(AuthenticationForm):
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username is not None and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                try:
+                    user_temp = User.objects.get(username=username)
+                except:
+                    user_temp = None
+                
+                if user_temp is not None:
+                    self.confirm_login_allowed(user_temp)
+                else:
+                    raise forms.ValidationError(
+                        self.error_messages['invalid_login'],
+                        code='invalid_login',
+                        params={'username': self.username_field.verbose_name},
+                    )
+
+        return self.cleaned_data
