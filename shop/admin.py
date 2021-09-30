db537d5b686b0384453ce67ad1ea8f211a955ca9
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-30 21:32:29 +0530

    Order model created and registered with admin

--- shop/admin.py
+++ shop/admin.py
@@ -7,6 +7,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'available']
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date', 'ref_id', 'total', 'status']
+
+
 
 
 admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Order, OrderAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -19,4 +20,22 @@ class Product(models.Model):
 
     def get_all_available_products():
         return Product.objects.filter(available='YES').order_by('name')
-    
+        
+
+class Order(models.Model):
+    # first value is the value in the database and second one is the human-readable name
+    ORDER_STATUS = [
+        ("CART", "in cart"),
+        ("PLACED", "order placed"),
+        ("DONE", "completed")
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="Customer")
+    # to set the status accordingly... cart functionality is from this field... use get_or_create() for new cart or existing cart
+    status = models.CharField(max_length=15, choices=ORDER_STATUS, default=ORDER_STATUS[0][1], blank=False, verbose_name="Status")
+    # date is set to empty until the order is placed
+    date = models.DateField(blank=True, null=True)
+    # payment reference id for future use in case   .. the type of field is not yet sure, so setting it to a char field
+    ref_id = models.CharField(max_length=100, blank=True, default="")
+    # store the total amount only after placing the order
+    total = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
